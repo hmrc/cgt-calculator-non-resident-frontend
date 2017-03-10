44aed6b4049142757ccdde14aeda0a2ac27b82c1
mac.grewal <mac.grewal@digital.hmrc.gov.uk>
2017-03-10 11:20:46 +0000

    Updated headers to reflect the correct copyright year of 2017

--- app/controllers/HowBecameOwnerController.scala
+++ app/controllers/HowBecameOwnerController.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 HM Revenue & Customs
+ * Copyright 2017 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -81,4 +81,3 @@ trait HowBecameOwnerController extends FrontendController with ValidActiveSessio
     howBecameOwnerForm.bindFromRequest.fold(errorAction, successAction)
   }
 }
-
