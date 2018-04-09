83e2dc093b7d8f2ce6d2d937579e8952aef9d21f
Kieran Puaar <Kieran.Puaar@digital.hmrc.gov.uk>
2018-04-09 09:23:15 +0100

    Adding User banner

--- public/javascripts/cgt.js
+++ public/javascripts/cgt.js
@@ -56,4 +56,18 @@ $(document).ready($(function () {
     ga('send', 'event','non-resident-get-help', 'Get help' , reportLocation);
     });
 
+// =====================================================
+// Handle the CGT UR panel dismiss link functionality
+// =====================================================
+    var cookieData=GOVUK.getCookie("mdtpurr");
+    if (cookieData==null) {
+        $("#ur-panel").addClass("banner-panel--show");
+    }
+
+    $(".banner-panel__close").on("click", function(e) {
+        e.preventDefault();
+         GOVUK.setCookie("mdtpurr", "suppress_for_all_services", 99999999999);
+         $("#ur-panel").removeClass("banner-panel--show");
+    });
+
 }));
