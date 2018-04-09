c8a49973095c36cb1d262b0b8ebe8896b8c854c7
Paul Anderson <paul.anderson@digital.hmrc.gov.uk>
2018-04-09 17:36:59 +0100

    DL-614-2 replicate assets FE setCookie

--- public/javascripts/cgt.js
+++ public/javascripts/cgt.js
@@ -59,6 +59,24 @@ $(document).ready($(function () {
 // =====================================================
 // Handle the CGT UR panel dismiss link functionality
 // =====================================================
+    function setCookie (name, value, duration, domain) {
+        var secure = window.location.protocol.indexOf('https') ? '' : '; secure'
+
+        var cookieDomain = ""
+        if (domain) {
+          cookieDomain = '; domain=' + domain
+        }
+
+        var expires = ""
+        if (duration) {
+          var date = new Date()
+          date.setTime(date.getTime() + (duration * 24 * 60 * 60 * 1000))
+          expires = '; expires=' + date.toGMTString()
+        }
+
+        document.cookie = name + '=' + value + expires + cookieDomain + '; path=/' + secure
+    }
+
     var cookieData=GOVUK.getCookie("mdtpurr");
     if (cookieData==null) {
         $("#ur-panel").addClass("banner-panel--show");
@@ -66,7 +84,7 @@ $(document).ready($(function () {
 
     $(".banner-panel__close").on("click", function(e) {
         e.preventDefault();
-         GOVUK.setCookie("mdtpurr", "suppress_for_all_services", 99999999999);
+        setCookie("mdtpurr", "suppress_for_all_services", 30);
          $("#ur-panel").removeClass("banner-panel--show");
     });
 
