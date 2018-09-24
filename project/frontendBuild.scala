7c235d6d601ccb2c936df4adc90f83bf9218c27f
Kieran Puaar <Kieran.Puaar@digital.hmrc.gov.uk>
2018-09-24 15:35:44 +0100

    DL-1242 - Migrating to new build

--- project/frontendBuild.scala
+++ project/frontendBuild.scala
@@ -37,7 +37,7 @@ private object AppDependencies {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "frontend-bootstrap" % "10.1.0",
+    "uk.gov.hmrc" %% "frontend-bootstrap" % "10.4.0",
     "uk.gov.hmrc" %% "play-partials" % "6.1.0",
     "uk.gov.hmrc" %% "http-caching-client" % "7.1.0",
     "uk.gov.hmrc" %% "mongo-caching" % "5.4.0",
@@ -54,7 +54,7 @@ private object AppDependencies {
   object Test {
     def apply(): Seq[ModuleID] = new TestDependencies {
       override lazy val test = Seq(
-        "uk.gov.hmrc" %% "hmrctest" % "3.0.0" % scope,
+        "uk.gov.hmrc" %% "hmrctest" % "3.1.0" % scope,
         "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.0" % scope,
         "org.mockito" % "mockito-core" % "2.13.0" % scope,
         "org.pegdown" % "pegdown" % "1.6.0" % scope,
