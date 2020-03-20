c2315235ec8dfb8eb4226f8229957bf82a81a511
AndrewRButcher <6122384+AndrewRButcher@users.noreply.github.com>
2020-03-20 13:44:11 +0000

    DL-2411 revert mongo chaching

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -25,7 +25,7 @@ object AppDependencies {
     "uk.gov.hmrc" %% "bootstrap-play-26" % "1.5.0",
     "uk.gov.hmrc" %% "play-partials" % "6.9.0-play-26",
     "uk.gov.hmrc" %% "http-caching-client" % "9.0.0-play-26",
-    "uk.gov.hmrc" %% "mongo-caching" % "6.10.0-play-26",
+    "uk.gov.hmrc" %% "mongo-caching" % "6.8.0-play-26",
     "uk.gov.hmrc" %% "play-language" % "4.2.0-play-26",
     "it.innove"   % "play2-pdf" % "1.8.2" exclude("com.typesafe.play","*"),
     "uk.gov.hmrc" %% "govuk-template" % "5.52.0-play-26",
