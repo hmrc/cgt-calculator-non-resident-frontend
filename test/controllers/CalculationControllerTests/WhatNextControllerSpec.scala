396eef383f7e2d7f41a079ef86d493a9651fbd39
AliKarami <karamiali19@gmail.com>
2020-08-10 19:32:35 +0100

    DL-3943 - Applied policy changes on reporting

--- test/controllers/CalculationControllerTests/WhatNextControllerSpec.scala
+++ test/controllers/CalculationControllerTests/WhatNextControllerSpec.scala
@@ -19,25 +19,56 @@ package controllers.CalculationControllerTests
 import akka.stream.Materializer
 import assets.MessageLookup
 import config.ApplicationConfig
+import constructors.AnswersConstructor
 import controllers.WhatNextController
 import controllers.helpers.FakeRequestHelper
+import models._
 import org.jsoup.Jsoup
+import org.mockito.ArgumentMatchers
 import org.scalatestplus.mockito.MockitoSugar
 import play.api.mvc.MessagesControllerComponents
 import play.api.test.Helpers._
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.play.bootstrap.http.DefaultHttpClient
 import uk.gov.hmrc.play.test.{UnitSpec, WithFakeApplication}
+import org.mockito.Mockito._
+import scala.concurrent.Future
 
 class WhatNextControllerSpec extends UnitSpec with WithFakeApplication with MockitoSugar with FakeRequestHelper {
 
   implicit val hc: HeaderCarrier = mock[HeaderCarrier]
+  val mockHttp = mock[DefaultHttpClient]
   val mockConfig = fakeApplication.injector.instanceOf[ApplicationConfig]
   lazy val materializer = mock[Materializer]
   val mockMessagesControllerComponents = fakeApplication.injector.instanceOf[MessagesControllerComponents]
+  val mockAnswersConstructor = mock[AnswersConstructor]
+
+  def setupTarget(summary: TotalGainAnswersModel): WhatNextController = {
+    when(mockAnswersConstructor.getNRTotalGainAnswers(ArgumentMatchers.any()))
+      .thenReturn(Future.successful(summary))
+
+    new WhatNextController(http = mock[DefaultHttpClient], mockAnswersConstructor, mockConfig, fakeApplication, mockMessagesControllerComponents)
+  }
+
+  lazy val answerModel = TotalGainAnswersModel(
+    DateModel(6, 4, 2020),
+    SoldOrGivenAwayModel(false),
+    None,
+    DisposalValueModel(0),
+    DisposalCostsModel(0),
+    None,
+    None,
+    AcquisitionValueModel(0),
+    AcquisitionCostsModel(0),
+    DateModel(7, 4, 2020),
+    None,
+    None,
+    ImprovementsModel("No", None, None),
+    None
+  )
 
   "Calling .whatNext" when {
-    lazy val target = new WhatNextController(http = mock[DefaultHttpClient], mockConfig, fakeApplication, mockMessagesControllerComponents){}
+    lazy val target = setupTarget(answerModel)
     "provided with a valid request" should {
       lazy val result = target.whatNext(fakeRequestWithSession)
       lazy val document = Jsoup.parse(bodyOf(result)(materializer))
@@ -48,6 +79,7 @@ class WhatNextControllerSpec extends UnitSpec with WithFakeApplication with Mock
 
       "load the whatNext page" in {
         document.title() shouldBe MessageLookup.WhatNext.title
+        document.body().select("a.button").attr("href") shouldBe "/capital-gains-tax-uk-property/start/report-pay-capital-gains-tax-uk-property"
       }
     }
 
